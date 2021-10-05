7d95c95ac381b96cd000fc2588df4720475ae60f
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-10-05 16:09:19 -0700

    edit cofig to deploy to heroku

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
